bff0f554998f1263dfe94ef8fa9029b6325346fe
CkFreak <5blaesche@informatik.uni-hamburg.de>
2017-03-16 12:22:36 +0100

    Created constructor and class comment

--- src/main/java/services/NoteService.java
+++ src/main/java/services/NoteService.java
@@ -2,7 +2,13 @@ package services;
 
 /**
  * Created by Timbo on 16/03/2017.
+ *
+ * The NoteService handles Notes from users. It also offers acces to operations of the Note class.
  */
 public class NoteService
 {
+    public NoteService()
+    {
+
+    }
 }
