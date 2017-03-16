c1ff33ef6ce51cb2298a851502d4451ded72a4bd
CkFreak <5blaesche@informatik.uni-hamburg.de>
2017-03-16 11:48:59 +0100

    Created new classes for the note System

---
+++ src/main/java/materials/Note.java
@@ -0,0 +1,8 @@
+package materials;
+
+/**
+ * Created by Timbo on 16/03/2017.
+ */
+public class Note
+{
+}
---
+++ src/main/java/services/NoteService.java
@@ -0,0 +1,8 @@
+package services;
+
+/**
+ * Created by Timbo on 16/03/2017.
+ */
+public class NoteService
+{
+}
