879f792f5fcfbe791306d84fd53a3e32ba402ca5
CkFreak <5blaesche@informatik.uni-hamburg.de>
2017-03-19 12:38:07 +0100

    Finished the constructor and added the add method

--- src/main/java/services/NoteService.java
+++ src/main/java/services/NoteService.java
@@ -1,14 +1,48 @@
 package services;
 
+import materials.Note;
+import net.dv8tion.jda.core.entities.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Timbo on 16/03/2017.
  *
- * The NoteService handles Notes from users. It also offers acces to operations of the Note class.
+ * The NoteService handles Notes from users. It also offers access to operations of the Note class.
  */
 public class NoteService
 {
-    public NoteService()
-    {
+    private List<Note> _notes;
 
+    public NoteService(List<User> serverUsers)
+    {
+        _notes = new ArrayList<>();
+        initNoteList(serverUsers);
+    }
+
+    public boolean addNote(User author, String note)
+    {
+        for (Note remark : _notes)
+        {
+            if (remark.getAuthor().equals(author))
+            {
+                remark.addNote(note);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Initialzes the note list of this class
+     * @param serverUsers all the users from the server this note list belongs to.
+     */
+    private void initNoteList(List<User> serverUsers)
+    {
+        for (User user : serverUsers)
+        {
+            _notes.add(Note.getNoteForUser(user));
+        }
     }
 }
