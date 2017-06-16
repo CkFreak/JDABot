69c14b600b381082d54f8ab70a91c0b9395d9c09
CkFreak <5blaesche@informatik.uni-hamburg.de>
2017-06-16 14:29:53 +0200

    Automated token and jda instance creation

--- src/main/java/start/StartUp.java
+++ src/main/java/start/StartUp.java
@@ -17,57 +17,29 @@ import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.Game;
 import net.dv8tion.jda.core.exceptions.RateLimitedException;
 
+/**
+ * Starts the bot and all its instances up
+ * @version 06.2017
+ */
 public class StartUp
 {
-    private final static String MCI_SERVER_TOKEN = provideToken(0);
-    private final static String MOGE_SERVER_TOKEN = provideToken(1);
-    private final static String AQUILA_SERVER = provideToken(2);
-    private final static String UNICORNPORN_SERVER = provideToken(3);
     private final static String PATH_TO_TOKEN = "src/main/res/tokens.txt";
 
     public static void main(String[] args)
     {
-        JDA mciJda;
-        JDA mogeJda;
-        JDA aquilaJda;
-        JDA unicornJda;
+        JDA jda;
         try
         {
 
-            //Build the MCI JDA instance
-            mciJda = new JDABuilder(AccountType.BOT).setToken(MCI_SERVER_TOKEN).buildBlocking();
-            mciJda.addEventListener(new ReadyListener());
-            mciJda.addEventListener(new CommandListener(mciJda));
-            mciJda.addEventListener(new UserPromotedListener());
-            mciJda.addEventListener(new MessageListener());
-            mciJda.getPresence().setGame(Game.of("Hello Kitty Online"));
-
-            //Build the moge JDA instance
-            mogeJda = new JDABuilder(AccountType.BOT).setToken(MOGE_SERVER_TOKEN).buildBlocking();
-            mogeJda.addEventListener(new ReadyListener());
-            mogeJda.addEventListener(new CommandListener(mogeJda));
-            mogeJda.addEventListener(new UserPromotedListener());
-            mogeJda.addEventListener(new MessageListener());
-            mogeJda.getPresence().setGame(Game.of("DOTA 2"));
-
-            //Build the aquila JDA instance
-            aquilaJda = new JDABuilder(AccountType.BOT).setToken(AQUILA_SERVER).buildBlocking();
-            aquilaJda.addEventListener(new ReadyListener());
-            aquilaJda.addEventListener(new CommandListener(aquilaJda));
-            aquilaJda.addEventListener(new UserPromotedListener());
-            aquilaJda.addEventListener(new MessageListener());
-            aquilaJda.getPresence().setGame(Game.of("League of Legends"));
-
-            //Build the unicorn JDA instance
-            unicornJda = new JDABuilder(AccountType.BOT).setToken(UNICORNPORN_SERVER).buildBlocking();
-            unicornJda.addEventListener(new ReadyListener());
-            unicornJda.addEventListener(new CommandListener(unicornJda));
-            unicornJda.addEventListener(new UserPromotedListener());
-            unicornJda.addEventListener(new MessageListener());
-            unicornJda.getPresence().setGame(Game.of("Ben and Ed Bloodparty"));
-
-
-
+            for (String string  : Files.readAllLines(Paths.get(PATH_TO_TOKEN)))
+            {
+                jda = new JDABuilder(AccountType.BOT).setToken(string).buildBlocking();
+                jda.addEventListener(new ReadyListener());
+                jda.addEventListener(new CommandListener(jda));
+                jda.addEventListener(new UserPromotedListener());
+                jda.addEventListener(new MessageListener());
+                jda.getPresence().setGame(Game.of("Hello Kitty Online"));
+            }
 
         }
         catch (LoginException | IllegalArgumentException
@@ -76,25 +48,9 @@ public class StartUp
             System.out.println("An Exception during the startup process has been raised check internet connection!");
             e.printStackTrace();
         }
-    }
-
-    /**
-     * Returns the Token for a bot from a file located in the specified location
-     * @param count Specifies which token you want (in case there is more than one)
-     * @return The bot token corresponding to the count
-     */
-    private static String provideToken(int count)
-    {
-        try
-        {
-            List<String> readAllLines = Files.readAllLines(Paths.get(PATH_TO_TOKEN));
-            return readAllLines.get(count);
-        }
         catch (IOException e)
         {
-            System.out.println("Unfortunately there was a permission problem with the file you specified for the Bot Token :(");
             e.printStackTrace();
         }
-        return null;
     }
 }
