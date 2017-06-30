3b703a15e9adfcd2927dc334ff08e831177ad11d
CkFreak <5blaesche@informatik.uni-hamburg.de>
2017-06-30 20:29:50 +0200

    Implemented service

--- src/main/java/weather/WeatherService.java
+++ src/main/java/weather/WeatherService.java
@@ -1,5 +1,8 @@
 package weather;
 
+import net.dv8tion.jda.core.entities.Message;
+import org.json.simple.parser.ParseException;
+
 import java.io.IOException;
 
 /**
@@ -13,13 +16,41 @@ public class WeatherService
     public WeatherService()
     {
         _client = new WeatherClient();
-        try
-        {
-            _client.getWeatherDataFor("Hamburg");
-        }
-        catch (IOException e)
-        {
-            e.printStackTrace();
-        }
+    }
+
+    /**
+     * Gives a complete overview over the weather conditions
+     * @param location The city one want to know the weather information about
+     * @return A Message Object with all the weather information
+     * @throws IOException When OpenWeatherMap returns an error
+     * @throws ParseException When the JSON Object cannot be parsed correctly
+     */
+    public Message getCompleteWeatherData(String location) throws IOException, ParseException
+    {
+        return _client.getCompleteFormattedWeatherFor(location);
+    }
+
+    /**
+     * Gives the temperature information for a city
+     * @param location The city one wants to know the temperature for
+     * @return A Message Object with the temperature information
+     * @throws IOException When OpenWeatherMap returns an error
+     * @throws ParseException When the JSON Object cannot be parsed correctly
+     */
+    public Message getTemperatureData(String location) throws IOException, ParseException
+    {
+        return _client.getTemperatureDataFor(location);
+    }
+
+    /**
+     * Gives the wind data for a city
+     * @param location The city one wants to know the wind data for
+     * @return A Message Object with the wind information
+     * @throws IOException When OpenWeatherMap returns an error
+     * @throws ParseException When the JSON Object cannot be parsed correctly
+     */
+    public Message getWindData(String location) throws IOException, ParseException
+    {
+        return _client.getWindDataFor(location);
     }
 }
